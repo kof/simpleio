0c33bc1b9e217aeef4ba41ce01db9925049c3866
Oleg Slobodskoi <oleg008@gmail.com>
2013-10-04 17:49:49 +0200

    example can now send json as data

--- examples/send.js
+++ examples/send.js
@@ -5,15 +5,21 @@ var sio,
     Adapter,
     enabledUsers = ['aaa', 'bbb', 'ccc', 'ddd'];
 
+function json(str) {
+    try {
+        str = JSON.parse(str)
+    } catch(err) {}
+
+    return str;
+}
+
 program
     .option('-a, --adapter <adapter>', 'adapter to use Memory|Mongo', String, 'Memory')
-    .option('-d, --data <data>', 'data to send', String, 'test data')
+    .option('-d, --data <data>', 'data to send', json, 'test data')
     .option('-r, --recipient <recipient>', 'recipient defined in client', String)
     .option('-e, --event <event>', 'event the client listens to', String)
     .option('-A, --amount <amount>', 'amount of time to send the same event to test multiplexing', Number, 1)
     .parse(process.argv);
-
-
 
 Adapter = require('../lib/server/adapters/' + program.adapter);
 
